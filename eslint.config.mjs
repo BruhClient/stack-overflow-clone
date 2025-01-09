e90b31fb9e182796ef236835bbe97f9fb55351fd
Travis <Travisang40@gmail.com>
2025-01-09 22:09:52 +0800

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
- 
+  ...compat.config({
+    "@typescript-eslint/no-explicit-any": "off"
+  })
   
   
 ];
